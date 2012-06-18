256e9eb0a19742e5f1e1e8e171b8fc4949d10fdb
codyzhang <codyzhang.work@gmail.com>
2012-06-18 10:50:34 +0800

    rename

---
+++ src/main/db/mysql/myproject.sql
@@ -0,0 +1,70 @@
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: myproject
+Target Host: localhost
+Target Database: myproject
+Date: 2012-6-15 10:35:43
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for app_user
+-- ----------------------------
+CREATE TABLE `app_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `account_expired` bit(1) NOT NULL,
+  `account_locked` bit(1) NOT NULL,
+  `address` varchar(150) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `postal_code` varchar(15) DEFAULT NULL,
+  `province` varchar(100) DEFAULT NULL,
+  `credentials_expired` bit(1) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `account_enabled` bit(1) DEFAULT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `password_hint` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `version` int(11) DEFAULT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `description` varchar(64) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+CREATE TABLE `user_role` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `FK143BF46A4FD90D75` (`role_id`),
+  KEY `FK143BF46AF503D155` (`user_id`),
+  CONSTRAINT `FK143BF46AF503D155` FOREIGN KEY (`user_id`) REFERENCES `app_user` (`id`),
+  CONSTRAINT `FK143BF46A4FD90D75` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
+INSERT INTO `app_user` VALUES ('-2', '', '', '', 'Denver', 'US', '80210', 'CO', '', 'matt@raibledesigns.com', '', 'Matt', 'Raible', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Not a female kitty.', '', 'admin', '1', 'http://raibledesigns.com');
+INSERT INTO `app_user` VALUES ('-1', '', '', '', 'Denver', 'US', '80210', 'CO', '', 'matt_raible@yahoo.com', '', 'Tomcat', 'User', '12dea96fec20593566ab75692c9949596833adc9', 'A male kitty.', '', 'user', '1', 'http://tomcat.apache.org');
+INSERT INTO `role` VALUES ('-2', 'Default role for all Users', 'ROLE_USER');
+INSERT INTO `role` VALUES ('-1', 'Administrator role (can edit Users)', 'ROLE_ADMIN');
+INSERT INTO `user_role` VALUES ('-1', '-2');
+INSERT INTO `user_role` VALUES ('-2', '-1');
